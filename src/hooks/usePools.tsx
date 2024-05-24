d9f8ea40d29ee6b92a90b56db3a85e5b83fdf84a
JoseFredes <jose.fredes@mayor.cl>
2024-05-24 02:36:53 -0400

    feat: add usePoolAsset hook

--- src/hooks/usePools.tsx
+++ src/hooks/usePools.tsx
@@ -11,10 +11,12 @@ interface Pool {
   reserves: Array<{ asset: string; amount: string }>;
 }
 
+//TODO: delete when the another usePools is finished
+
 export function usePoolsForAsset(asset?: AssetType): Pool[] | undefined {
   const { serverHorizon } = useSorobanReact();
   const [pools, setPools] = useState<Pool[] | undefined>(undefined);
-
+  console.log("🚀 « asset:", asset);
   useEffect(() => {
     const fetchPoolsForAsset = async () => {
       try {
@@ -46,4 +48,56 @@ export function usePoolsForAsset(asset?: AssetType): Pool[] | undefined {
   }, [asset, serverHorizon]);
 
   return pools;
+}
+
+export function usePoolsTest3(asset: AssetType) {
+  const { serverHorizon } = useSorobanReact();
+  const [pools, setPools] = useState<Pool[]>([]);
+  const newAsset = new Asset(asset?.code, asset?.issuer);
+  const [next, setNext] = useState(() =>
+    serverHorizon?.liquidityPools().forAssets(newAsset).call()
+  );
+  const [loading, setLoading] = useState(false);
+
+  const loadMore = async () => {
+    if (loading || !next) {
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await next;
+
+      setPools((prevPools) => {
+        const newPools = response.records.map((record) => ({
+          id: record.id,
+          fee: record.fee_bp,
+          type: record.type,
+          total_shares: record.total_shares,
+          reserves: record.reserves.map((reserve) => ({
+            asset: reserve.asset,
+            amount: reserve.amount,
+          })),
+        }));
+
+        const uniquePools = Array.from(
+          new Set([
+            ...prevPools.map((pool) => JSON.stringify(pool)),
+            ...newPools.map((pool) => JSON.stringify(pool)),
+          ])
+        );
+
+        return uniquePools.map((pool) => JSON.parse(pool));
+      });
+      setNext(response.next);
+    } catch (error) {
+      console.error("Error loading pools:", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadMore();
+  }, []);
+  return { pools, loading, loadMore };
 }
