e86bfc2fdf554b76bacb3e04e3a823742281ae27
coderipper <hello@joaquinsoza.dev>
2024-11-01 18:21:48 -0300

    update

--- src/components/Assets/AssetCard.tsx
+++ src/components/Assets/AssetCard.tsx
@@ -13,9 +13,15 @@ import {
   Skeleton,
   useMediaQuery,
   Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
 } from "@chakra-ui/react";
 import { useMemo } from "react";
 import { BiCopy } from "react-icons/bi";
+import { IoChevronDown } from "react-icons/io5";
 
 interface AssetCardProps {
   name?: string;
@@ -38,38 +44,79 @@ export function AssetCard({
   return (
     <Card rounded={"2xl"} width={"100%"} variant="elevated">
       <CardBody>
-        <HStack alignItems={"center"}>
-          <Skeleton isLoaded={Boolean(icon)} fadeDuration={1} rounded={"full"}>
-            <Avatar name={name} src={icon} size={{ base: "lg", md: "2xl" }} />
-          </Skeleton>
-          <Box>
+        <HStack>
+          <HStack alignItems={"center"}>
             <Skeleton
-              height={{ base: "30px", md: 9 }}
-              isLoaded={Boolean(name)}
+              isLoaded={Boolean(icon)}
               fadeDuration={1}
+              rounded={"full"}
             >
-              <Flex alignItems={"baseline"} gap={2}>
-                <Text
-                  fontSize={{ base: "xl", md: "2xl" }}
-                  fontWeight="bold"
-                  whiteSpace="nowrap"
-                  overflow="hidden"
-                  textOverflow="clip"
+              <Avatar name={name} src={icon} size={{ base: "lg", md: "xl" }} />
+            </Skeleton>
+            <Box>
+              <Skeleton
+                height={{ base: "30px", md: 9 }}
+                isLoaded={Boolean(name)}
+                fadeDuration={1}
+              >
+                <Flex alignItems={"baseline"} gap={2}>
+                  <Text
+                    fontSize={{ base: "xl", md: "2xl" }}
+                    fontWeight="bold"
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="clip"
+                    textColor={"#333333"}
+                  >
+                    {name ?? "Asset name"}
+                  </Text>
+                  {name !== code && (
+                    <Text
+                      textColor={"#A3A3A3"}
+                      fontSize={{ base: "xs", md: "sm" }}
+                    >
+                      ({code})
+                    </Text>
+                  )}
+                </Flex>
+              </Skeleton>
+              <Skeleton isLoaded={Boolean(domain)} fadeDuration={1}>
+                <Link
+                  href={`https://${domain}`}
+                  textColor={"#A3A3A3"}
+                  target="_blank"
                 >
-                  {name ?? "Asset name"}
-                </Text>
-                {name !== code && (
-                  <Text fontSize={{ base: "xs", md: "sm" }}>({code})</Text>
-                )}
-              </Flex>
-            </Skeleton>
-            <Skeleton isLoaded={Boolean(domain)} fadeDuration={1}>
-              <Link href={`https://${domain}`} target="_blank">
-                {domain ?? "Asset Domain"}
-              </Link>
-            </Skeleton>
-          </Box>
-          <VStack
+                  {domain ?? "Asset Domain"}
+                </Link>
+              </Skeleton>
+            </Box>
+          </HStack>
+          <HStack>
+            <Box>
+              <VStack>
+                <HStack>
+                  <Menu>
+                    <MenuButton as={Button} rightIcon={<IoChevronDown />}>
+                      See on
+                    </MenuButton>
+                    <MenuList>
+                      <MenuItem>Download</MenuItem>
+                      <MenuItem>Create a Copy</MenuItem>
+                    </MenuList>
+                  </Menu>
+                  <Button
+                    colorScheme="pink"
+                    variant="solid"
+                    // onClick={onClick}
+                    // isLoading={isLoading}
+                  >
+                    Swap on Soroswap
+                  </Button>
+                </HStack>
+              </VStack>
+            </Box>
+          </HStack>
+          {/* <VStack
             align={"start"}
             gap={{ base: 0, md: 2 }}
             borderLeftWidth="2px"
@@ -92,7 +139,7 @@ export function AssetCard({
                 </HStack>
               </Tooltip>
             </Skeleton>
-          </VStack>
+          </VStack> */}
         </HStack>
       </CardBody>
     </Card>
